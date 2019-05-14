8670caba33b7c6159c390b744aebebffc23fedcd
Harry Hornbacher <harry.hornbacher@futurice.com>
2019-05-14 16:29:29 +0200

    implement setSpeed and setSteering

--- src/Control.cpp
+++ src/Control.cpp
@@ -9,3 +9,13 @@ void Control::begin()
     _speedCtrl.arm();
     _steeringCtrl.attach(_steeringPin);
 }
+
+void Control::setSpeed(uint16_t speed)
+{
+    _speedCtrl.speed(speed);
+}
+
+void Control::setSteering(uint16_t angle)
+{
+    _steeringCtrl.write(angle);
+}
