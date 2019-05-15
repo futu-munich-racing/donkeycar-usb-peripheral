b158de285ba449ee079edc57338bd7ef8ae5c729
Harry Hornbacher <harry.hornbacher@futurice.com>
2019-05-15 11:17:23 +0200

    refactoring & cleanup

--- client/carperipheral.py
+++ client/carperipheral.py
@@ -1,74 +1,44 @@
-import serial
-from sliplib import Driver as SLIPDriver
-from hexdump import hexdump
-from zlib import crc32
-import struct
-import time
-
-driver = SLIPDriver()
+from protocol import Protcol
+from threading import Timer
 
 
-def calcChecksum(data):
-    return (crc32(data) & 0xffffffff).to_bytes(4, 'little')
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
 
 
-def sendControlPacket(comm, steering, speed):
-    print('Steering: {0}, Speed: {1}'.format(steering, speed))
-    payload = bytearray(struct.pack('<HH', steering, speed))
-    checksum = calcChecksum(payload)
-    payload.extend(checksum)
-    framed = driver.send(payload)
-    hexdump(payload)
-    comm.write(framed)
-
-
-class SensorMessage:
-    def __init__(self, data):
-        payload = data[0:24]
-        checksum = data[24:28]
-        calculatedChecksum = calcChecksum(payload)
-
-        self.valid = checksum == calculatedChecksum
-        if not self.valid:
-            return
-
-        tmp = list(struct.unpack('<HHHHHHHHHHHH', payload))
-
-        self.distance = tmp[0:3]
-        self.compass = tmp[3:6]
-        self.acceleration = tmp[6:9]
-        self.gyro = tmp[9:12]
-
-    def __str__(self):
-        if not self.valid:
-            return 'SensorMessage: Invalid!'
-        return 'SensorMessage:\nDist: {0}\nCompass: {1}\nAccel: {2}\nGyro: {3}'.format(
-            self.distance,
-            self.compass,
-            self.acceleration,
-            self.gyro
-        )
-
-
-inBuff = bytearray()
+proto = Protcol()
 
 counter = 0
+def send():
+    global counter
+    proto.sendControlPacket(counter, counter)
+    counter = counter + 30
+    if counter >= 180:
+        counter = 0
 
-with serial.Serial('/dev/ttyACM0', 115200) as comm:
-    while True:
-        inBuff.extend(comm.read_all())
 
-        if len(inBuff) >= 28:
-            data = driver.receive(inBuff)
-            if len(data) > 0:
-                for packet in data:
-                    print(len(packet))
-                    hexdump(packet)
-                    msg = SensorMessage(packet)
-                    print(msg)
+rt = RepeatedTimer(1, send)
 
-        time.sleep(1)
-        sendControlPacket(comm, counter, counter)
-        counter = counter + 60
-        if counter > 180:
-            counter = 0
+proto.start()
--- client/protocol.py
+++ client/protocol.py
@@ -44,82 +44,54 @@ class SensorPacket:
         )
 
 
-class Writer(asyncio.Protocol):
-    def connection_made(self, transport):
-        """Store the serial transport and schedule the task to send data.
-        """
-        self._transport = transport
-        print('Writer connection created')
-        # asyncio.ensure_future(self.send())
-        # print('Writer.send() scheduled')
-
-    def connection_lost(self, exc):
-        print('Writer closed')
-
-    async def send(self, packet):
-        """Send four newline-terminated messages, one byte at a time.
-        """
-        # message = b'foo\nbar\nbaz\nqux\n'
-        # for b in message:
-        #     await asyncio.sleep(0.5)
-        #     self.transport.serial.write(bytes([b]))
-        #     print(f'Writer sent: {bytes([b])}')
-        # self.transport.close()
-
-
-class Reader(asyncio.Protocol):
+class Serial(asyncio.Protocol):
     def __init__(self, packetHandler=lambda: None):
         super().__init__()
         self._packetHandler = packetHandler
 
     def connection_made(self, transport):
-        """Store the serial transport and prepare to receive data.
-        """
         self._transport = transport
         self._buffer = bytearray()
-        print('Reader connection created')
+
+    def connection_lost(self, exc):
+        print('Writer closed')
+
+    async def send(self, packet):
+        self._transport.serial.write(packet)
 
     def data_received(self, data):
-        """Store characters until a newline is received.
-        """
         print('Data received')
         self._buffer += data
         if len(self._buffer) >= 28:
-            data = slipDriver.receive(self._buffer)
+            try:
+                data = slipDriver.receive(self._buffer)
+            except:
+                pass
             if len(data) > 0:
                 for rawPacket in data:
-                    hexdump(rawPacket)
+                    # hexdump(rawPacket)
                     packet = SensorPacket(rawPacket)
                     self._packetHandler(packet)
-        # if b'\n' in self.buf:
-        #     lines = self.buf.split(b'\n')
-        #     self.buf = lines[-1]  # whatever was left over
-        #     for line in lines[:-1]:
-        #         print(f'Reader received: {line.decode()}')
-        #         self.msgs_recvd += 1
-        # if self.msgs_recvd == 4:
-        #     self.transport.close()
-
-    def connection_lost(self, exc):
-        print('Reader closed')
 
 
 class Protcol:
     def __init__(self, device='/dev/ttyACM0', baud=115200):
         self._loop = asyncio.get_event_loop()
-        self._reader = serial_asyncio.create_serial_connection(
-            self._loop, lambda: Reader(self.packetHandler), device, baudrate=baud)
-        # self._writer = serial_asyncio.create_serial_connection(self._loop, Writer, device, baudrate=baud)
-        asyncio.ensure_future(self._reader)
-        print('Reader scheduled')
-        # asyncio.ensure_future(self._writer)
-        # print('Writer scheduled')
+        self._serialProtocol = Serial(self.packetHandler)
+        self._serialConnection = serial_asyncio.create_serial_connection(
+            self._loop, lambda: self._serialProtocol, device, baudrate=baud)
+        asyncio.ensure_future(self._serialConnection)
 
     def start(self):
-        # self._loop.call_later(10, self._loop.stop)
         self._loop.run_forever()
-        print('Done')
 
     def packetHandler(self, packet):
-        print(packet)
+        # print(packet)
+        pass
 
+    def sendControlPacket(self, steering, speed):
+        print('Steering: {0}, Speed: {1}'.format(steering, speed))
+        payload = bytearray(struct.pack('<HH', steering, speed))
+        checksum = calcChecksum(payload)
+        payload.extend(checksum)
+        self._loop.create_task(self._serialProtocol.send(slipDriver.send(payload)))
