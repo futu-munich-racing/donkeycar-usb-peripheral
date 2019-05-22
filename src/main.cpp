40f241fb98700e005eaa3040eb556fa90e6d8f02
Harry Hornbacher <harry.hornbacher@futurice.com>
2019-05-22 11:13:00 +0200

    make heartbeat non-blocking

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,26 @@ Control control(PB9, PB8);
 Protocol protocol(control);
 Sensors sensors(PB12, PB13, PB14, PB15, protocol);
 
+uint32_t lastLedToggle{0};
+
 void setup()
 {
-    pinMode(PC13, OUTPUT);
-    digitalWrite(PC13, LOW);
+  pinMode(PC13, OUTPUT);
+  digitalWrite(PC13, LOW);
 
-    protocol.begin(115200);
-    control.begin();
-    sensors.begin();
+  protocol.begin(115200);
+  control.begin();
+  sensors.begin();
 }
 
 void loop()
 {
-    sensors.update();
-    protocol.update();
+  sensors.update();
+  protocol.update();
 
-    delay(150);
+  if (millis() > lastLedToggle + 700)
+  {
     digitalToggle(PC13);
+    lastLedToggle = millis();
+  }
 }
