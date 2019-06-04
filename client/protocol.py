ef220c5d052eae3dede0ba22b8d302affd4e621b
Harry Hornbacher <harry.hornbacher@futurice.com>
2019-06-05 00:03:39 +0200

    adopt to new packet struct

--- client/protocol.py
+++ client/protocol.py
@@ -1,10 +1,6 @@
-# Sources from: https://tinkering.xyz/async-serial/
-# Thanks for the great article!
-
 import asyncio
 import serial_asyncio
 from sliplib import Driver as SLIPDriver
-from hexdump import hexdump
 from zlib import crc32
 import struct
 
@@ -18,33 +14,34 @@ slipDriver = SLIPDriver()
 
 class SensorPacket:
     def __init__(self, data):
-        if not isinstance(data, bytes):
+        if isinstance(data, int):
             self.valid = False
             return
-            
-        payload = data[0:24]
-        checksum = data[24:28]
+        payload = data[0:28]
+        checksum = data[28:32]
         calculatedChecksum = calcChecksum(payload)
 
         self.valid = checksum == calculatedChecksum
         if not self.valid:
             return
 
-        tmp = list(struct.unpack('<HHHHHHHHHHHH', payload))
+        tmp = list(struct.unpack('<HHHHHHHHHHHHBBBB', payload))
 
         self.distance = tmp[0:3]
-        self.magneto = tmp[3:6]
+        self.orientation = tmp[3:6]
         self.acceleration = tmp[6:9]
-        self.gyro = tmp[9:12]
+        self.rotation = tmp[9:12]
+        self.calibration = tmp[12:16]
 
     def __str__(self):
         if not self.valid:
             return 'SensorMessage: Invalid!'
-        return 'SensorMessage:\nDist: {0}\nCompass: {1}\nAccel: {2}\nGyro: {3}'.format(
+        return 'SensorMessage:\nDist: {0}\nOrientation: {1}\nAccel: {2}\nRotation: {3}\nCalibration: {4}'.format(
             self.distance,
-            self.magneto,
+            self.orientation,
             self.acceleration,
-            self.gyro
+            self.rotation,
+            self.calibration
         )
 
 
@@ -57,14 +54,7 @@ class Serial(asyncio.Protocol):
         self._transport = transport
         self._buffer = bytearray()
 
-    def connection_lost(self, exc):
-        print('Writer closed')
-
-    async def send(self, packet):
-        self._transport.serial.write(packet)
-
     def data_received(self, data):
-        print('Data received')
         self._buffer += data
         if len(self._buffer) >= 28:
             try:
@@ -73,38 +63,18 @@ class Serial(asyncio.Protocol):
                 pass
             if len(data) > 0:
                 for rawPacket in data:
-                    # hexdump(rawPacket)
                     packet = SensorPacket(rawPacket)
                     if packet.valid:
                         self._packetHandler(packet)
 
 
 class Protocol:
-    def __init__(self, device='/dev/ttyACM0', baud=115200, packetHandler=lambda: None):
+    def __init__(self, device='/dev/ttyAMA0', baud=115200, packetHandler=lambda: None):
         self._loop = asyncio.get_event_loop()
         self._serialProtocol = Serial(packetHandler)
         self._serialConnection = serial_asyncio.create_serial_connection(
             self._loop, lambda: self._serialProtocol, device, baudrate=baud)
         asyncio.ensure_future(self._serialConnection)
 
-        self._steering = 0
-        self._throttle = 0
-
     def start(self):
         self._loop.run_forever()
-
-    def setSteering(self, steering):
-        self._steering = steering
-        self._sendControlPacket(self._steering, self._throttle)
-
-    def setThrottle(self, throttle):
-        self._throttle = throttle
-        self._sendControlPacket(self._steering, self._throttle)
-
-    def _sendControlPacket(self, steering, throttle):
-        print('Steering: {0}, Speed: {1}'.format(steering, throttle))
-        payload = bytearray(struct.pack('<HH', steering, throttle))
-        checksum = calcChecksum(payload)
-        payload.extend(checksum)
-        self._loop.create_task(
-            self._serialProtocol.send(slipDriver.send(payload)))
